07203b67448260963d70fabb9dc698cd8426f7f7
Sebastian Peters <sebastian.peters@tib.uni-hannover.de>
2012-04-16 18:27:24 +0200

    add HtmlTest

---
+++ src/test/java/org/datacite/schema/test/HtmlTest.java
@@ -0,0 +1,58 @@
+package org.datacite.schema.test;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.apache.commons.exec.CommandLine;
+import org.apache.commons.exec.DefaultExecutor;
+import org.apache.commons.exec.ExecuteException;
+import org.apache.commons.io.FileUtils;
+import org.datacite.schema.test.junit.LabeledParameterized;
+import org.datacite.schema.test.junit.LabeledParameterized.Parameters;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Html Validator. Requires external program "tidy" (http://tidy.sourceforge.net/).
+ * 
+ * @author peterss
+ *
+ */
+@RunWith(LabeledParameterized.class)
+public class HtmlTest {
+
+    private File file;
+
+    public HtmlTest(File file) {
+        this.file = file;
+    }
+
+    @Test
+    public void testIndexHtmlIsValid() throws ExecuteException, IOException {
+        CommandLine cmdLine = CommandLine.parse("tidy");
+        cmdLine.addArgument(file.getAbsolutePath());
+        DefaultExecutor executor = new DefaultExecutor();
+        int exitValue = executor.execute(cmdLine);
+        assertEquals(0, exitValue);
+    }
+
+
+    @Parameters
+    public static Collection<Object[]> data() {
+        File directory = new File("www/");
+        Collection<Object[]> data = new ArrayList<Object[]>();
+        Collection<File> files = FileUtils.listFiles(directory, new String[] { "html" }, true);
+        for (File file : files)
+            data.add(new Object[] { file });
+        return data;
+    }
+    
+    @Override
+    public String toString() {
+        return file.toString();
+    }
+}
