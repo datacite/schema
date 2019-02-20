085a989a952dfaa22434785e592af6c0a02ce714
Martin Fenner <martin.fenner@datacite.org>
2019-02-20 17:33:23 +0100

    don't check DOI pattern or identifierType DOI. #43

--- spec/kernel-4/doi_spec.rb
+++ spec/kernel-4/doi_spec.rb
@@ -16,25 +16,4 @@ describe "validate DOI" do
     errors = xsd.validate(Nokogiri::XML(doc.to_xml)).map { |error| error.to_s }
     expect(errors).to be_empty
   end
-
-  it '8.5555/123' do
-    doc.at("identifier").content = "8.5555/123"
-    errors = xsd.validate(Nokogiri::XML(doc.to_xml)).map { |error| error.to_s }
-    expect(errors.length).to eq(2)
-    expect(errors.first).to include("not accepted by the pattern '10\\..+/.+'")
-  end
-
-  it '10.5555' do
-    doc.at("identifier").content = "10.5555"
-    errors = xsd.validate(Nokogiri::XML(doc.to_xml)).map { |error| error.to_s }
-    expect(errors.length).to eq(2)
-    expect(errors.first).to include("not accepted by the pattern '10\\..+/.+'")
-  end
-
-  it '123' do
-    doc.at("identifier").content = "123"
-    errors = xsd.validate(Nokogiri::XML(doc.to_xml)).map { |error| error.to_s }
-    expect(errors.length).to eq(2)
-    expect(errors.first).to include("not accepted by the pattern '10\\..+/.+'")
-  end
 end
