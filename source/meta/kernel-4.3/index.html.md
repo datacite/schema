5dfded8578ce78e9f6cf168bc8139d225b5803f3
Martin Fenner <martin.fenner@datacite.org>
2019-07-13 09:19:09 +0200

    draft schema 4.3. #56

---
+++ source/meta/kernel-4.3/index.html.md
@@ -0,0 +1,41 @@
+---
+title: DataCite Metadata Schema 4.3
+version: 4.3
+date: 2019-07-13
+layout: schema
+schema_url: /meta/kernel-4.3/metadata.xsd
+---
+
+## Documentation
+DataCite Metadata Working Group. (2019). DataCite Metadata Schema Documentation for the Publication and Citation of Research Data. Version 4.3. DataCite e.V.
+
+
+## Schema
+DataCite Metadata Working Group. (2019). DataCite Metadata Schema for the Publication and Citation of Research Data. Version 4.3. DataCite e.V.
+
+<a href="metadata.xsd" class="btn">View XSD</a>
+
+## Changes
+
+* Addition of optional "affiliationIdentifier", "affiliationIdentifierScheme", and "schemeURI" for affiliation
+* Addition of optional "schemeURI" for funderIdentifier
+* Addition of "ROR" to allowed values for funderIdentifierType
+
+## Examples
+
+* [Full DataCite XML example](example/datacite-example-full-v4.xml)
+* [Example for affiliation](example/datacite-example-affiliation-v4.xml)
+* [Example for software](example/datacite-example-software-v4.xml)
+* [Example for a data paper](example/datacite-example-datapaper-v4.xml)
+* [Example for a simple dataset](example/datacite-example-dataset-v4.xml)
+* [Example with complicated values](example/datacite-example-complicated-v4.xml)
+* [Example with DataCollector as Contributor and a geoLocation box](example/datacite-example-Box_dateCollected_DataCollector-v4.xml)
+* [Example with GeoLocation](example/datacite-example-GeoLocation-v4.xml)
+* [Example with Polygon](example/datacite-example-polygon-v4.xml)
+* [Example with HasMetadata as related resource](example/datacite-example-HasMetadata-v4.xml)
+* [Example with IsIdenticalTo as related resource](example/datacite-example-relationTypeIsIdenticalTo-v4.xml)
+* [Example with ResearchGroup as Contributor and Methods as Description](example/datacite-example-ResearchGroup_Methods-v4.xml)
+* [Example with Collection as ResourceType](example/datacite-example-ResourceTypeGeneral_Collection-v4.xml)
+* [Example with Video as ResourceType](example/datacite-example-video-v4.xml)
+* [Example for a workflow ResourceType](example/datacite-example-workflow-v4.xml)
+* [Example with fundingReference](example/datacite-example-fundingReference-v.4.xml)
---
+++ spec/kernel-4.3/other_spec.rb
@@ -0,0 +1,65 @@
+require 'spec_helper'
+
+describe "validate other elements" do
+  let(:root) { File.join(File.dirname(__FILE__), '../../source/meta/kernel-4') }
+  let(:xsd) { Dir.chdir(root) { Nokogiri::XML::Schema(File.read("metadata.xsd")) }}
+  let(:doc) { Dir.chdir(root) { Nokogiri::XML(File.read("example/datacite-example-full-v4.xml")) { |c| c.strict }}}
+
+  describe "resourceType" do
+    it 'missing resourceType' do
+      element = doc.at("resourceType")
+      element.replace ""
+      errors = xsd.validate(Nokogiri::XML(doc.to_xml)).map { |error| error.to_s }
+      expect(errors.length).to eq(1)
+      expect(errors.first).to include("Missing child element(s). Expected is ( {http://datacite.org/schema/kernel-4}resourceType ).")
+    end
+
+    it 'missing resourceTypeGeneral' do
+      element = doc.at("resourceType")
+      element.replace "<resourceType/>"
+      errors = xsd.validate(Nokogiri::XML(doc.to_xml)).map { |error| error.to_s }
+      expect(errors.length).to eq(1)
+      expect(errors.first).to include("Element '{http://datacite.org/schema/kernel-4}resourceType': The attribute 'resourceTypeGeneral' is required but missing.")
+    end
+
+    it 'missing resourceTypeGeneral with resourceType' do
+      element = doc.at("resourceType")
+      element.replace '<resourceType>Dataset</resourceType>'
+      errors = xsd.validate(Nokogiri::XML(doc.to_xml)).map { |error| error.to_s }
+      expect(errors.length).to eq(1)
+      expect(errors.first).to include("Element '{http://datacite.org/schema/kernel-4}resourceType': The attribute 'resourceTypeGeneral' is required but missing.")
+    end
+
+    it 'empty resourceTypeGeneral with resourceType' do
+      element = doc.at("resourceType")
+      element.replace '<resourceType resourceTypeGeneral="">Dataset</resourceType>'
+      errors = xsd.validate(Nokogiri::XML(doc.to_xml)).map { |error| error.to_s }
+      expect(errors.length).to eq(2)
+      expect(errors.last).to eq("35:0: ERROR: Element '{http://datacite.org/schema/kernel-4}resourceType', attribute 'resourceTypeGeneral': '' is not a valid value of the atomic type '{http://datacite.org/schema/kernel-4}resourceType'.")
+    end
+
+    it 'empty resourceType' do
+      element = doc.at("resourceType")
+      element.replace '<resourceType resourceTypeGeneral="Dataset"></resourceType>'
+      errors = xsd.validate(Nokogiri::XML(doc.to_xml)).map { |error| error.to_s }
+      expect(errors).to be_empty
+    end
+
+    it 'unknown resourceTypeGeneral' do
+      element = doc.at("resourceType")
+      element.replace '<resourceType resourceTypeGeneral="Conference">Dataset</resourceType>'
+      errors = xsd.validate(Nokogiri::XML(doc.to_xml)).map { |error| error.to_s }
+      expect(errors.length).to eq(2)
+      expect(errors.first).to include("The value 'Conference' is not an element of the set {'Audiovisual', 'Collection', 'DataPaper', 'Dataset', 'Event', 'Image', 'InteractiveResource', 'Model', 'PhysicalObject', 'Service', 'Software', 'Sound', 'Text', 'Workflow', 'Other'}.")
+    end
+  end
+
+  describe "description" do
+    it 'empty descriptions tag' do
+      element = doc.at("descriptions")
+      element.replace "<descriptions/>"
+      errors = xsd.validate(Nokogiri::XML(doc.to_xml)).map { |error| error.to_s }
+      expect(errors).to be_empty
+    end
+  end
+end
