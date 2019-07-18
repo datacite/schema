e9da9cd80d48e1d02729e6267fdea74ea4a2aad8
Martin Fenner <martin.fenner@datacite.org>
2019-07-18 20:33:17 +0200

    use different example. #56

--- spec/kernel-4/pure_example_spec.rb
+++ spec/kernel-4/pure_example_spec.rb
@@ -9,32 +9,32 @@ describe "pure example" do
     expect(doc).to pass_validation(xsd)
   end
 
-  it 'has resourceTypeGeneral Software' do
+  it 'has resourceTypeGeneral Dataset' do
     resource_type = doc.at("resourceType")
-    expect(resource_type["resourceTypeGeneral"]).to eq("Audiovisual")
+    expect(resource_type["resourceTypeGeneral"]).to eq("Dataset")
     expect(resource_type.text).to eq("Dataset")
   end
 
   it 'has givenName, familyName, and affiliations' do
     creators = doc.search("creator")
-    expect(creators.size).to eq(14)
+    expect(creators.size).to eq(1)
     creator = creators.first
     expect(creator.elements.size).to eq(4)
 
     creator_name = creator.elements[0]
     expect(creator_name.name).to eq("creatorName")
-    expect(creator_name.text).to eq("Haywood, Raphaelle Dawn")
+    expect(creator_name.text).to eq("Barbosa, Miguel")
 
     given_name = creator.elements[1]
     expect(given_name.name).to eq("givenName")
-    expect(given_name.text).to eq("Raphaelle Dawn")
+    expect(given_name.text).to eq("Miguel")
 
     family_name = creator.elements[2]
     expect(family_name.name).to eq("familyName")
-    expect(family_name.text).to eq("Haywood")
+    expect(family_name.text).to eq("Barbosa")
 
     affiliation = creator.elements[3]
     expect(affiliation.name).to eq("affiliation")
-    expect(affiliation.text).to eq("School of Physics and Astronomy")
+    expect(affiliation.text).to eq("School of Biology")
   end
 end
