ad35dd6baa0e059ac260f5590758692406f5b78f
codycooperross <50597551+codycooperross@users.noreply.github.com>
2025-11-10 15:26:33 +0100

    Add tests for Schema 4.7 and correct XSD typo

---
+++ spec/kernel-4.7/example_full_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+describe "full example" do
+  let(:root) { File.join(File.dirname(__FILE__), '../../source/meta/kernel-4') }
+  let(:xsd) { Dir.chdir(root) { Nokogiri::XML::Schema(File.read("metadata.xsd")) }}
+  let(:doc) { Dir.chdir(root) { Nokogiri::XML(File.read("example/datacite-example-full-v4.xml")) { |c| c.strict }}}
+
+  it 'validates' do
+    expect(doc).to pass_validation(xsd)
+  end
+
+  it 'has givenName and familyName' do
+    creators = doc.search("/xmlns:resource/xmlns:creators/xmlns:creator")
+    expect(creators.size).to eq(2)
+    creator = creators.first
+    expect(creator.elements.size).to eq(5)
+
+    creator_name = creator.elements[0]
+    expect(creator_name.name).to eq("creatorName")
+    expect(creator_name.text).to eq("ExampleFamilyName, ExampleGivenName")
+
+    given_name = creator.elements[1]
+    expect(given_name.name).to eq("givenName")
+    expect(given_name.text).to eq("ExampleGivenName")
+
+    family_name = creator.elements[2]
+    expect(family_name.name).to eq("familyName")
+    expect(family_name.text).to eq("ExampleFamilyName")
+
+    name_identifier = creator.elements[3]
+    expect(name_identifier.name).to eq("nameIdentifier")
+    expect(name_identifier["schemeURI"]).to eq("https://orcid.org")
+    expect(name_identifier["nameIdentifierScheme"]).to eq("ORCID")
+    expect(name_identifier.text).to eq("https://orcid.org/0000-0001-5727-2427")
+  end
+end
